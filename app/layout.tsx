0c5521655c7f10803595489ebaf6a1394d54267e
akshaySolsten <akshay@solsten.in>
2026-06-11 13:43:35 +0530

    Suppress extension hydration warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,8 +33,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>
         <PostHogRuntime>
           {children}
           <PwaRuntime />
